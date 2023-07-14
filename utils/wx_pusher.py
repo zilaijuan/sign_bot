257f99463ac79ffbd893b3ab8846629b2fa51926
zhouwanjun <zhouwj86@chinaunicom.cn>
2023-07-14 09:25:16 +0800

    update

--- utils/wx_pusher.py
+++ utils/wx_pusher.py
@@ -1,11 +1,11 @@
 from wxpusher import WxPusher
-from config import WXPUSHER_APPTOKEN
+import os
 
 def push_to_wechat(text,summary=''):
     WxPusher.send_message(text,
                           summary=summary,
                           uids=['UID_NY2XaP87yCnZhFQqg30a5yYBWvLs'],
-                          token=WXPUSHER_APPTOKEN)
+                          token=os.environ["WXPUSHER_APPTOKEN"])
 
 if __name__ == '__main__':
     push_to_wechat(text = 'this is a test message', summary="summary")
